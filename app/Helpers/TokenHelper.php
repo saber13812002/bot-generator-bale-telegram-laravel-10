e76e4103e72158a246f25fbba036dafe87700181
Saber Tabatabaee Yazdi <saber.tabatabaee@gmail.com>
2023-04-11 17:52:52 +0330

    ساختار توکن های تلگرام پشتیبانی شود

--- app/Helpers/TokenHelper.php
+++ app/Helpers/TokenHelper.php
@@ -14,7 +14,11 @@ class TokenHelper
         $text = str_replace("\n", '', $text); // remove new lines
         $text = str_replace("\r", '', $text);
 
-        $check = preg_match("/^[0-9]{8,10}:[a-zA-Z0-9_-]{40}$/", $text);
+        if ($type == 'bale') {
+            $check = preg_match("/^[0-9]{8,10}:[a-zA-Z0-9_-]{40}$/", $text);
+        } else {
+            $check = preg_match("/^[0-9]{8,10}:[a-zA-Z0-9_-]{35}$/", $text);
+        }
 //        dd($check);
         if ($check) {
             return true;
@@ -24,9 +28,11 @@ class TokenHelper
 
 
     /**
+     * @param string $type
      * @return mixed
      */
-    public static function getMotherBotToken($type = 'telegram'): mixed
+    public
+    static function getMotherBotToken(string $type = 'telegram'): mixed
     {
         $bot_token = env('BOT_MOTHER_TOKEN' . ($type == 'telegram' ? '_TELEGRAM' : '_BALE'));
         if ($bot_token == null) {
