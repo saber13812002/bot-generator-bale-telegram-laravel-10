20da69baaed1b7076829b50ab8a9ea761f4a7c1c
saber <saber.tabatabaee@gmail.com>
2023-04-12 18:47:28 +0330

    add translation for spanish french dutch chinese phrases for quran muslim islam holy book . telegram bot.agregue la traducción para el español, el francés, el holandés, las frases chinas para el libro sagrado del Islam musulmán del Corán. robot de telegrama. ajouter la traduction pour les phrases espagnoles françaises néerlandaises chinoises pour le livre saint de l'islam musulman coran. robot de télégramme. robot de télégramme. 為古蘭經穆斯林伊斯蘭教聖書添加法語荷蘭語中文短語的翻譯。電報機器人. voeg vertaling toe voor duitse nederlandse zinnen voor quran telegram bot

---
+++ lang/es/passwords.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'reset'     => 'Su contraseña ha sido restablecida.',
+    'sent'      => 'Te hemos enviado por correo electrónico el enlace para restablecer tu contraseña.',
+    'throttled' => 'Por favor, espere antes de reintentar.',
+    'token'     => 'Este token de restablecimiento de contraseña no es válido.',
+    'user'      => 'No podemos encontrar un usuario con esa dirección de correo electrónico.',
+];
---
+++ lang/es/validation.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'current_password' => 'La contraseña es incorrecta.',
+    'custom'           => [
+        'attribute-name' => [
+            'rule-name' => 'mensaje personalizado',
+        ],
+    ],
+];
