08bbc0ef037cdbedd693f02637598f4b6292183f
saber <saber.tabatabaee@gmail.com>
2023-04-12 18:55:07 +0330

    add a translation for Russian Urdo Spanish french dutch Chinese Turkish phrases for Quran Muslim Islam Holy Book. Telegram bot. добавить перевод для русского урдо, испанского, французского, голландского, китайского, турецкого, фраз для Корана, мусульманского ислама, Священной книги. Телеграмм бот  Kur'an-ı Kerim Müslüman İslam Kutsal Kitap için Rusça İspanyolca Fransızca Hollandaca Çince Türkçe ifadeler için bir çeviri ekleyin. telgraf botu . agregue una traducción para el español, francés, holandés, chino, turco, frases para el Libro Sagrado del Islam musulmán del Corán. robot de telegrama. ajouter la traduction pour les phrases espagnoles françaises néerlandaises chinoises pour le livre saint de l'islam musulman coran. robot de télégramme. robot de télégramme. 為古蘭經穆斯林伊斯蘭教聖書添加法語荷蘭語中文短語的翻譯。電報機器人. voeg vertaling toe voor duitse nederlandse zinnen voor quran telegram bot. Kur'an-ı Kerim Müslüman İslam Kutsal Kitap için İspanyolca fransızca hollandaca çince türkçe ifadeler için bir çeviri ekleyin. telgraf botu قرآن مسلم اسلام مقدس کتاب کے لیے روسی اردو ہسپانوی فرانسیسی ڈچ چینی ترک جملے کا ترجمہ شامل کریں۔ ٹیلیگرام بوٹ

---
+++ lang/ur/validation.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'current_password' => 'پاس ورڈ غلط ہے۔',
+    'custom'           => [
+        'attribute-name' => [
+            'rule-name' => 'حسب ضرورت پیغام',
+        ],
+    ],
+];
