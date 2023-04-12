1d03377f6fde3034a958ce886d91478c253f486a
saber <saber.tabatabaee@gmail.com>
2023-04-12 18:45:46 +0330

    add translation for french dutch chinese phrases for quran muslim islam holy book telegram bot. ajouter la traduction pour les phrases chinoises néerlandaises françaises pour le livre saint de l'islam musulman du coran. robot de télégramme. 為古蘭經穆斯林伊斯蘭教聖書添加法語荷蘭語中文短語的翻譯。電報機器人. voeg vertaling toe voor duitse nederlandse zinnen voor quran telegram bot

---
+++ lang/fr/pagination.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'previous' => '&laquo;  Précédent',
+    'next'     => 'Suivant  &raquo;',
+];
---
+++ lang/fr/validation.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'confirmed'        => 'La confirmation du champ :attribute ne correspond pas.',
+    'current_password' => 'Le mot de passe est incorrect.',
+    'custom'           => [
+        'attribute-name' => [
+            'rule-name' => 'message personnalisé',
+        ],
+    ],
+];
