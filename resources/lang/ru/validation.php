ee544a646cb6ade1a079b52fb9c1f6346a06365e
saber <saber.tabatabaee@gmail.com>
2023-04-12 12:32:13 +0330

    ترجمه کلمات به روسی و تست

---
+++ resources/lang/ru/validation.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'accepted'        => 'Поле :attribute должно быть принято, если :other равно :value.',
+    'accepted_if'     => 'Поле :attribute должно быть принято, если :other равно :value.',
+    'active_url'      => 'Поле :attribute должно быть принято, если :other равно :value.',
+    'after'           => 'Поле :attribute должно быть принято, если :other равно :value.',
+    'after_or_equal'  => 'Поле :attribute должно быть датой после или равной :date.',
+    'alpha'           => 'Поле :attribute должно быть принято, если :other равно :value.',
+    'alpha_dash'      => 'Поле :attribute должно содержать только буквы, цифры, тире и знаки подчеркивания.',
+    'alpha_num'       => 'Поле :attribute должно содержать только буквы, цифры, тире и знаки подчеркивания.',
+    'array'           => 'Поле :attribute должно быть датой после или равной :date.',
+    'ascii'           => 'Поле :attribute должно быть принято, если :other равно :value.',
+    'before'          => 'Поле :attribute должно быть датой после или равной :date.',
+    'before_or_equal' => 'Поле :attribute должно быть датой после или равной :date.',
+    'between'         => [
+        'array'   => 'Поле :attribute должно содержать элементы от :min до :max.',
+        'file'    => 'Поле :attribute должно быть размером между :min и :max килобайтами.',
+        'numeric' => 'Поле :attribute должно находиться между :min и :max.',
+        'string'  => 'Поле :attribute должно находиться между символами :min и :max.',
+    ],
+    'boolean'         => 'Поле :attribute должно быть истинным или ложным.',
+    'confirmed'       => 'Подтверждение поля :attribute не совпадает.',
+];
