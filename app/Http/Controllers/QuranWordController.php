b35e42ac5ffd55c750fd311922122b421ccf59a9
saber <saber.tabatabaee@gmail.com>
2023-04-22 23:03:56 +0330

    move log into helper

--- app/Http/Controllers/QuranWordController.php
+++ app/Http/Controllers/QuranWordController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\BotHelper;
+use App\Helpers\LogHelper;
 use App\Helpers\QuranHefzBotHelper;
 use App\Http\Requests\BotRequest;
 use App\Http\Requests\StoreQuranWordRequest;
 use App\Http\Requests\UpdateQuranWordRequest;
-use App\Models\BotLog;
 use App\Models\QuranSurah;
 use App\Models\QuranWord;
 use Exception;
@@ -47,7 +47,7 @@ class QuranWordController extends Controller
             }
 
             try {
-                $this->log($request, $type, $bot);
+                LogHelper::log($request, $type, $bot);
             } catch (Exception $e) {
                 Log::info($e->getMessage());
             }
@@ -334,31 +334,5 @@ class QuranWordController extends Controller
         }
     }
 
-    /**
-     * @param BotRequest $request
-     * @param mixed $type
-     * @param Telegram $bot
-     * @return void
-     */
-    public function log(BotRequest $request, mixed $type, Telegram $bot): void
-    {
-        $log = new BotLog();
-        $log->webhook_endpoint_uri = request()->segment(2);
-        $log->bot_mother_id = 0;
-        $log->language = $request->input('language');
-        $log->locale = App::getLocale();
-        $log->type = $type;
-        $log->text = substr($bot->Text(), 0, 19);
-        $log->is_command = substr($bot->Text(), 0, 1) === "/";
-        $log->channel_group_type = $bot->ChatID() < 0 ? $bot->ChatID() : 0;
-        $log->bot_id = 1;
-        $log->chat_id = $bot->ChatID();
-//        $log->message_id = 0;// $bot->MessageID();
-//        $log->from_id = 0;// $bot->FromID() ?? "";
-//        $log->from_chat_id = 0;// $bot->FromChatID() ?? "";
-
-//        dd($log->attributesToArray());
-        $log->save();
-    }
 
 }
