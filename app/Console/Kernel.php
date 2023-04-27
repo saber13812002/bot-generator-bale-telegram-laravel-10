a0426dac8125a943bf0f1f2e89e78c264dbd3938
Saber Tabatabaee Yazdi <saber.tabatabaee@gmail.com>
2023-04-27 17:36:05 +0330

    report daily bot logs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\TaskReminderCommand;
 use App\Console\Commands\weatherWindCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -15,6 +16,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command(weatherWindCommand::class, ['speed=12'])->dailyAt("6:30"); //10:00 iran
+        $schedule->command(TaskReminderCommand::class)->dailyAt("20:28"); //10:00 iran
 //        $schedule->command(weatherWindCommand::class, ['speed=12'])->everyFiveMinutes();
     }
 
