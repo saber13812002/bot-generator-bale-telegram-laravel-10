813aab469d9f9118699693d870fa900377a7cd85
saber <saber.tabatabaee@gmail.com>
2023-06-05 23:06:40 +0330

    quran bot v5.81.4 add transliteration model

---
+++ app/Models/QuranTransliterationEn.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuranTransliterationEn extends Model
+{
+    use HasFactory;
+}
