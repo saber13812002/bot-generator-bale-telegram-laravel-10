a672e2f4762b44d1c3c8334c0b6894d09e4aae5b
saber <saber.tabatabaee@gmail.com>
2023-06-05 23:06:09 +0330

    quran bot v5.81.3 add transliteration migration

---
+++ database/migrations/2023_06_05_193119_create_quran_transliteration_trs_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quran_transliteration_trs', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('quran_transliteration_en', 1100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quran_transliteration_trs');
+    }
+};
---
+++ database/migrations/2023_06_05_193134_create_quran_transliteration_ens_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quran_transliteration_ens', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('quran_transliteration_tr', 1000);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quran_transliteration_ens');
+    }
+};
