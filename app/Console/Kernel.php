8e69a85285324978e53fe5ec933965b256a5b690
saber <saber.tabatabaee@gmail.com>
2023-06-12 00:14:46 +0330

    quran bot v5.83.0 daily report for every user compare with yesterday
    move all user report to thursdays

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\TaskReminderCommand;
+use App\Console\Commands\UsersRankingCommand;
 use App\Console\Commands\weatherWindCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -16,8 +17,11 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command(weatherWindCommand::class, ['speed=7'])->dailyAt("6:30"); //10:00 iran
-        $schedule->command(TaskReminderCommand::class)->dailyAt("20:29"); //23:59 iran
+        $schedule->command(TaskReminderCommand::class)->thursdays("20:28"); //23:58 iran
 //        $schedule->command(weatherWindCommand::class, ['speed=12'])->everyFiveMinutes();
+
+                $schedule->command(UsersRankingCommand::class)->dailyAt("20:29"); //23:59 iran
+
     }
 
     /**
