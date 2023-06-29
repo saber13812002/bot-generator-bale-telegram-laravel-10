63c90a18463b84d1db29343f1ac83db219bc32d7
saber <saber.tabatabaee@gmail.com>
2023-06-29 23:33:58 +0330

    quran bot v5.88 add highlight message and bold test search

--- app/Helpers/QuranHefzBotHelper.php
+++ app/Helpers/QuranHefzBotHelper.php
@@ -314,8 +314,8 @@ https://quran.inoor.ir/fa/search/?query=" . $botText . "
      */
     public static function getHighlightMarker(string $type): array
     {
-        $start = $type == "bale" ? "*" : "<b>";
-        $end = $type == "bale" ? "*" : "</b>";
+        $start = $type == "bale" ? "*" : "**";
+        $end = $type == "bale" ? "*" : "**";
         return array($start, $end);
     }
 
