ff50db7ea86376c51cf673bc516263736bb0305a
Saber Tabatabaee Yazdi <saber.tabatabaee@gmail.com>
2023-07-09 17:07:39 +0330

    بروز رسانی صفحه اول سایت با خدمات جدید

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -916,6 +916,66 @@
                 </a>
 
 
+
+
+
+                <a href="https://t.me/hamsaraan_bot"
+                   class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                    <div>
+                        <div
+                            class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 class="w-7 h-7 stroke-red-500">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"/>
+                            </svg>
+                        </div>
+
+                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Rel Bot</h2>
+
+                        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                            https://t.me/hamsaraan_bot
+                        </p>
+                    </div>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/>
+                    </svg>
+                </a>
+
+
+
+                <a href="https://t.me/channel_admin_sync_news"
+                   class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                    <div>
+                        <div
+                            class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 class="w-7 h-7 stroke-red-500">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"/>
+                            </svg>
+                        </div>
+
+                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Sync Bot</h2>
+
+                        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                            sync your Bale Telegram Eitaa Channel - and twitter facebook instagram linkedin - https://ble.ir/channel_admin_sync_news
+                        </p>
+                    </div>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/>
+                    </svg>
+                </a>
+
+
+
+
             </div>
         </div>
 
