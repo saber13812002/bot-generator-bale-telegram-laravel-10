974434ad484f3ee001e4022042470f71ee8bdea0
saber <saber.tabatabaee@gmail.com>
2023-07-15 00:02:08 +0330

    quran bot v5.94 translation hebrew

--- lang/he/validation.php
+++ lang/he/validation.php
@@ -1,4 +1,17 @@
 <?php
 return [
-    'accepted' => 'יש לאשר את השדה :attribute.',
+    'accepted'       => 'יש לאשר את השדה :attribute.',
+    'accepted_if'    => 'יש לקבל את השדה :attribute כאשר :other הוא :value.',
+    'active_url'     => 'השדה :attribute חייב להיות כתובת אתר חוקית.',
+    'after'          => 'יש לקבל את השדה :attribute כאשר :other הוא :value.',
+    'after_or_equal' => 'יש לקבל את השדה :attribute כאשר :other הוא :value.',
+    'alpha'          => 'יש לקבל את השדה :attribute כאשר :other הוא :value.',
+    'alpha_dash'     => 'השדה :attribute חייב להכיל רק אותיות, מספרים, מקפים וקווים תחתונים.',
+    'alpha_num'      => 'יש לאשר את השדה :attribute.',
+    'array'          => 'יש לקבל את השדה :attribute כאשר :other הוא :value.',
+    'ascii'          => 'השדה :attribute חייב להכיל רק אותיות, מספרים, מקפים וקווים תחתונים.',
+    'before'         => 'השדה :attribute חייב להיות כתובת אתר חוקית.',
+    'between'        => [
+        'array' => 'השדה :attribute חייב להיות כתובת אתר חוקית.',
+    ],
 ];
