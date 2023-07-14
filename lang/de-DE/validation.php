c163e84d2b05efe37811b92e5970b4af1f6a615e
saber <saber.tabatabaee@gmail.com>
2023-07-14 19:20:23 +0330

    quran bot v5.94 translation german

--- lang/de-DE/validation.php
+++ lang/de-DE/validation.php
@@ -1,4 +1,9 @@
 <?php
 return [
     'current_password' => 'Das Passwort ist falsch.',
+    'custom'           => [
+        'attribute-name' => [
+            'rule-name' => 'benutzerdefinierte Nachricht',
+        ],
+    ],
 ];
