c32462d4c75bc8a010f9471cb632f5236f502334
saber <saber.tabatabaee@gmail.com>
2023-07-14 15:56:16 +0330

    quran bot v5.93 add command list of commands
    test command report bot users
    
    refactor to use those  command with same code
    
    add some phrase to translation

--- lang/ar-IQ/validation.php
+++ lang/ar-IQ/validation.php
@@ -1,5 +1,7 @@
 <?php
 return [
+    'accepted'         => 'يجب قبول الحقل :attribute.',
+    'accepted_if'      => 'يجب قبول الحقل :attribute عندما يكون :other هو :value.',
     'current_password' => 'كلمة المرور غير صحيحة.',
     'custom'           => [
         'attribute-name' => [
