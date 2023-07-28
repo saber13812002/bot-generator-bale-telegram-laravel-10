ead12b785e55b783d8b28ca3d6b4cf7a504f9e27
saber <saber.tabatabaee@gmail.com>
2023-07-28 17:14:41 +0330

    quran bot v6.020 get chat id bot for all messengers

---
+++ database/migrations/2023_07_28_134052_modify_origin_field_enum_to_blog_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('blog_users', function (Blueprint $table) {
+            $table->enum('type', ['bale', 'telegram', 'gap', 'soroosh'])->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('blog_users', function (Blueprint $table) {
+            $table->enum('type', ['bale', 'telegram'])->nullable()->change();
+        });
+    }
+};
