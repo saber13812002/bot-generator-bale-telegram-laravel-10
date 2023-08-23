b9dadab2f84f697eac82b388ff4fcfad5a0a4264
saber <saber.tabatabaee@gmail.com>
2023-08-23 23:51:05 +0330

    quran bot v6.7.1 ترجمه بوسنی قرآن

---
+++ lang/bs/passwords.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'reset'     => 'Vaša lozinka je poništena.',
+    'sent'      => 'Poslali smo e-poštom vašu vezu za poništavanje lozinke.',
+    'throttled' => 'Molimo pričekajte prije ponovnog pokušaja.',
+    'token'     => 'Ovaj token za poništavanje lozinke je nevažeći.',
+    'user'      => 'Ne možemo pronaći korisnika sa tom adresom e-pošte.',
+];
---
+++ lang/bs/validation.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'accepted'         => 'Polje :attribute mora biti prihvaćeno.',
+    'accepted_if'      => 'Polje :attribute mora biti prihvaćeno kada je :other :value.',
+    'current_password' => 'Lozinka je netačna.',
+    'custom'           => [
+        'attribute-name' => [
+            'rule-name' => 'prilagođena poruka',
+        ],
+    ],
+];
