4b8b3952564853e4f6f8d8897c07a35d9680d245
saber <saber.tabatabaee@gmail.com>
2023-10-20 22:49:12 +0330

    v6.14 hadith bot search all hadith translation system add porteghali for brazil and porteghal

--- config/translation.php
+++ config/translation.php
@@ -3,8 +3,7 @@
 return [
     'key' => env('TRANSLATIONIO_KEY'),
     'source_locale' => 'en',
-    'target_locales' => ['ar-IQ', 'az', 'bs', 'zh-CN', 'fr', 'de-DE', 'he', 'fa', 'ru', 'es', 'tr', 'ur'],
-
+    'target_locales' => ['ar-IQ', 'az', 'bs', 'zh-CN', 'fr', 'de-DE', 'he', 'fa', 'pt-BR', 'pt-PT', 'ru', 'es', 'tr', 'ur'],
     /* Directories to scan for Gettext strings */
     'gettext_parse_paths' => ['app', 'resources']
 ];
