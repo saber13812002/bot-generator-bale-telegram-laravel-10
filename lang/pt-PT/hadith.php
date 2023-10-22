e29e2af7f009c759c177a8ec97c09a42ed366081
saber <saber.tabatabaee@gmail.com>
2023-10-22 22:32:02 +0330

    v6.17 hadith bot fix save hadith then when request fetch and send to user

---
+++ lang/pt-PT/hadith.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'Please send your phrase to search in all shia hadith books' => [
+        '' => 'Por favor, envie a sua frase para pesquisar em todos os livros de hadith xiitas.',
+    ],
+    'in the name of God '                                        => [
+        ' you can use /help command to start' => [
+            '' => 'em nome de Deus . pode utilizar o comando /help para iniciar.',
+        ],
+    ],
+    'command not found'                                          => [
+        ' please send me /search to search all Shia Hadith then send your phrase to search' => [
+            '' => 'o comando não foi encontrado. Por favor, envie-me /search para pesquisar todos os Hadiths xiitas e depois envie a sua frase para pesquisar.',
+        ],
+    ],
+    'for more result click this link:'                           => 'para mais resultados, clique nesta hiperligação:',
+];
---
+++ lang/pt-PT/passwords.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'reset'     => 'A sua palavra-passe foi reposta.',
+    'sent'      => 'Enviámos por e-mail a sua ligação de redefinição da palavra-passe.',
+    'throttled' => 'Aguarde antes de tentar novamente.',
+    'token'     => 'Este token de redefinição de palavra-passe é inválido.',
+    'user'      => 'Não conseguimos encontrar um utilizador com esse endereço de e-mail.',
+];
