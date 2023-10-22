ca0e0f91e767b8dc4a9c352c93633e5cdf4afb38
saber <saber.tabatabaee@gmail.com>
2023-10-22 22:33:10 +0330

    v6.17 hadith bot fix save hadith then when request fetch and send to user

---
+++ lang/pt-PT/auth.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'failed'   => 'Estas credenciais não correspondem aos nossos registos.',
+    'password' => 'A palavra-passe fornecida está incorrecta.',
+    'throttle' => 'Demasiadas tentativas de início de sessão. Por favor, tente novamente em :seconds segundos.',
+];
