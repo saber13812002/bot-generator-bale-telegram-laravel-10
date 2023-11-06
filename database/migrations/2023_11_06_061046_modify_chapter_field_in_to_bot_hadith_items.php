075b2e2341a2f92ab0e21c1e3afceb00737f6c27
Saber Tabatabaee Yazdi <saber.tabatabaee@gmail.com>
2023-11-06 09:45:26 +0330

    باگ نداشتن جا برای نگهداری چپتر
     String data, right truncated: 1406 Data too long for column 'chapter' at row 1

---
+++ database/migrations/2023_11_06_061046_modify_chapter_field_in_to_bot_hadith_items.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bot_hadith_items', function (Blueprint $table) {
+            $table->text("chapter")->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bot_hadith_items', function (Blueprint $table) {
+            $table->string("chapter")->change();
+        });
+    }
+};
