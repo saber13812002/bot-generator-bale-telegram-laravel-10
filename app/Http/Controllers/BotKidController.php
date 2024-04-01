6c44998258a33beea88763306a0aff07040f0b1f
saber <saber.tabatabaee@gmail.com>
2024-04-01 20:40:48 +0330

    bot kids table for find all kids bot all around the world

---
+++ app/Http/Controllers/BotKidController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BotKid;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBotKidRequest;
+use App\Http\Requests\UpdateBotKidRequest;
+
+class BotKidController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBotKidRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BotKid $botKid)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BotKid $botKid)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBotKidRequest $request, BotKid $botKid)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BotKid $botKid)
+    {
+        //
+    }
+}
