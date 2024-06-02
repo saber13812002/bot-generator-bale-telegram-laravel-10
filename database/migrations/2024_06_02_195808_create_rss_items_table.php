92ae4d82f3150e1ba789d4821b3259251e396115
saber <saber.tabatabaee@gmail.com>
2024-06-02 23:34:43 +0330

    rss items table with title description url url fields

---
+++ database/migrations/2024_06_02_195808_create_rss_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rss_items', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->string('url')->nullable();
+            $table->string('url_ifttt')->nullable();
+            $table->string('url_rss_dot_app')->nullable();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rss_items');
+    }
+};
