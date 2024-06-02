527b97157fad26e96340a510f1724bbe6e30df31
saber <saber.tabatabaee@gmail.com>
2024-06-03 02:25:55 +0330

    rss post item model

---
+++ app/Models/RssPostItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RssPostItem extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2024_06_02_213350_create_rss_post_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rss_post_items', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('rss_item_id');
+
+            $table->string('title')->nullable();
+            $table->string('link')->unique();
+            $table->text('description')->nullable();
+            $table->dateTime('pub_date')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rss_post_items');
+    }
+};
