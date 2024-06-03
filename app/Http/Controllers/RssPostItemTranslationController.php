85acce33bc591cb096242aef7fff58a2a5073537
saber <saber.tabatabaee@gmail.com>
2024-06-03 11:44:55 +0330

    RssPostItemTranslation.php php artisan make:model -all

---
+++ app/Http/Controllers/RssPostItemTranslationController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RssPostItemTranslation;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreRssPostItemTranslationRequest;
+use App\Http\Requests\UpdateRssPostItemTranslationRequest;
+
+class RssPostItemTranslationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRssPostItemTranslationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RssPostItemTranslation $rssPostItemTranslation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RssPostItemTranslation $rssPostItemTranslation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRssPostItemTranslationRequest $request, RssPostItemTranslation $rssPostItemTranslation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RssPostItemTranslation $rssPostItemTranslation)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_06_03_071202_create_rss_post_item_translations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rss_post_item_translations', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger(`rss_post_item_id`);
+            $table->bigInteger(`author_chat_id`)->nullable();
+            $table->string(`locale`, 8)->default('fa');
+
+            $table->string(`title`);
+            $table->string(`content`);
+
+            $table->bigInteger('approved_by_chat_id')->nullable();
+            $table->boolean('approved');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rss_post_item_translations');
+    }
+};
