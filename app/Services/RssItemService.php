288ac6315e832beda4fe74ee739e5b41fa413fed
saber <saber.tabatabaee@gmail.com>
2024-06-03 12:04:20 +0330

    RssItemService.php cleanup

--- app/Services/RssItemService.php
+++ app/Services/RssItemService.php
@@ -13,12 +13,10 @@ class RssItemService
 
     public static function run()
     {
-
         $items = RssItem::query()->get();
 
         foreach ($items as $item) {
             $unique_field_name = $item->unique_xml_tag ?? 'link';
-
             $response = RssService::readRssAndSave($item->url, $item->id, $unique_field_name);
 //            dd($response);
         }
