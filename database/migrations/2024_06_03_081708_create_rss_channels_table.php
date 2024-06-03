212b8029a307f1a10089ecc2455686e9c1f1daaa
saber <saber.tabatabaee@gmail.com>
2024-06-03 12:01:20 +0330

    RssChannel.php php artisan make:model -a

---
+++ database/migrations/2024_06_03_081708_create_rss_channels_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rss_channels', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('token');
+            $table->string('target_id');
+            $table->enum('type', ['unknown', 'group', 'channel', 'private']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rss_channels');
+    }
+};
