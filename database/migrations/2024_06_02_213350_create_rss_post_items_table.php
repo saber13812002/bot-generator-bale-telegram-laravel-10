7ba34cfff2b0e27a036bb5eeb704417b33414b4e
saber <saber.tabatabaee@gmail.com>
2024-06-04 00:28:42 +0330

    link field should change to 700 as length

--- database/migrations/2024_06_02_213350_create_rss_post_items_table.php
+++ database/migrations/2024_06_02_213350_create_rss_post_items_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,7 +16,7 @@ return new class extends Migration
             $table->bigInteger('rss_item_id');
 
             $table->string('title')->nullable();
-            $table->string('link')->unique();
+            $table->string('link', 700)->unique();
             $table->text('description')->nullable();
             $table->dateTime('pub_date')->nullable();
 
