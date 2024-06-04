485772b63f604b4e9c6eaa5e344c32a026d5287c
saber <saber.tabatabaee@gmail.com>
2024-06-04 18:01:29 +0330

    RssItemsTableSeeder.php

--- database/seeders/RssItemsTableSeeder.php
+++ database/seeders/RssItemsTableSeeder.php
@@ -14,26 +14,27 @@ class RssItemsTableSeeder extends Seeder
      */
     public function run()
     {
-
+        
 
         \DB::table('rss_items')->delete();
-
+        
         \DB::table('rss_items')->insert(array (
-            0 =>
+            0 => 
             array (
                 'id' => 1,
                 'title' => 'tooljet youtubr',
                 'description' => 'tooljet youtubr',
                 'url' => 'http://127.0.0.1:8001/comments/feed/6',
-//                'url' => 'https://www.youtube.com/channel/UCf1p2G5Z7fPpvlBPf4l2I1w',
                 'url_ifttt' => NULL,
                 'url_rss_dot_app' => 'https://rss.app/feeds/4fbMLZP15RoXxPSP.xml',
                 'is_active' => 1,
                 'unique_xml_tag' => 'guid',
+                'locale' => 'fa',
+                'target_locale' => 'fa',
                 'created_at' => '2024-06-02 20:54:16',
                 'updated_at' => '2024-06-02 21:04:20',
             ),
-            1 =>
+            1 => 
             array (
                 'id' => 2,
                 'title' => 'saber blog',
@@ -43,10 +44,12 @@ class RssItemsTableSeeder extends Seeder
                 'url_rss_dot_app' => NULL,
                 'is_active' => 1,
                 'unique_xml_tag' => 'guid',
+                'locale' => 'fa',
+                'target_locale' => 'fa',
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
-            2 =>
+            2 => 
             array (
                 'id' => 3,
                 'title' => 'saber blog post',
@@ -56,11 +59,88 @@ class RssItemsTableSeeder extends Seeder
                 'url_rss_dot_app' => NULL,
                 'is_active' => 1,
                 'unique_xml_tag' => 'guid',
+                'locale' => 'fa',
+                'target_locale' => 'fa',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'title' => 'saber blog virgool',
+                'description' => 'saber blog virgool',
+                'url' => 'https://virgool.io/feed/@saber.tabatabaee',
+                'url_ifttt' => NULL,
+                'url_rss_dot_app' => NULL,
+                'is_active' => 1,
+                'unique_xml_tag' => 'link',
+                'locale' => 'fa',
+                'target_locale' => 'fa',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'title' => 'Flowable | LinkedIn',
+                'description' => 'Flowable | LinkedIn',
+                'url' => 'https://rss.app/feeds/OlCBlCZgkAK9LJ1N.xml',
+                'url_ifttt' => NULL,
+                'url_rss_dot_app' => 'https://www.linkedin.com/company/flowable-group/',
+                'is_active' => 1,
+                'unique_xml_tag' => NULL,
+                'locale' => 'en',
+                'target_locale' => 'en',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'title' => 'Camunda | LinkedIn',
+                'description' => 'Camunda | LinkedIn',
+                'url' => 'https://rss.app/feeds/sYlAlp95l2PDLyW7.xml',
+                'url_ifttt' => NULL,
+                'url_rss_dot_app' => NULL,
+                'is_active' => 1,
+                'unique_xml_tag' => NULL,
+                'locale' => 'en',
+                'target_locale' => 'en',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'title' => 'Bizagi | LinkedIn',
+                'description' => 'Bizagi | LinkedIn',
+                'url' => 'https://rss.app/feeds/6LlkPysl9CCe1EX1.xml',
+                'url_ifttt' => NULL,
+                'url_rss_dot_app' => NULL,
+                'is_active' => 1,
+                'unique_xml_tag' => NULL,
+                'locale' => 'en',
+                'target_locale' => 'en',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            7 => 
+            array (
+                'id' => 8,
+                'title' => 'ProcessMaker | LinkedIn',
+                'description' => 'ProcessMaker | LinkedIn',
+                'url' => 'https://rss.app/feeds/zmMt3LJUphX8tTGw.xml',
+                'url_ifttt' => NULL,
+                'url_rss_dot_app' => NULL,
+                'is_active' => 1,
+                'unique_xml_tag' => NULL,
+                'locale' => 'en',
+                'target_locale' => 'en',
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
         ));
-
-
+        
+        
     }
 }
