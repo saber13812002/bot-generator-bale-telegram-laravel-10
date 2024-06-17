03493ac38191c63b7839a59df0508a0920126d12
saber <saber.tabatabaee@gmail.com>
2024-06-18 00:47:31 +0330

    multi admin

---
+++ database/migrations/2024_06_17_112134_create_rss_business_user_admins_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rss_business_user_admins', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('user_id');
+            $table->bigInteger('rss_business_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rss_business_user_admins');
+    }
+};
