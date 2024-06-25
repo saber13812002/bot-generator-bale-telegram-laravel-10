ca43f65483818a2f9693a6bc4011e7de3b1750d0
saber tabatabaee <saber.tabatabaee@gmail.com>
2024-06-25 19:48:11 +0330

    implement rss social linkedin get post as item convert to feed

---
+++ app/Models/RssSocialPost.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RssSocialPost extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2024_06_25_153031_create_rss_social_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rss_social_posts', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('rss_item_id')->nullable();
+            $table->string('title');
+            $table->text('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rss_social_posts');
+    }
+};
