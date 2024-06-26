75afd58781b5b6b7f84d9a96529d5ff0ae65f62e
saber <saber.tabatabaee@gmail.com>
2024-06-27 01:26:09 +0330

    set default value for rss_business_id in rss_items table

--- app/Models/RssBusiness.php
+++ app/Models/RssBusiness.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class RssBusiness extends Model
 {
     use HasFactory;
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'admin_user_id');
+    }
 }
