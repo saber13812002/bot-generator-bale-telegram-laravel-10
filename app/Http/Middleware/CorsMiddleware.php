bc4cf56d35913c8d137c001d3645cf8f3037a4de
saber <saber.tabatabaee@gmail.com>
2024-08-04 20:32:01 +0330

    cors enable

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param \Closure(Request): (Response) $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*') // Adjust this to restrict origins
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+}
