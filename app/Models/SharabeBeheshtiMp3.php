7332c1961edc901e26f1c0c33724f208e85fd205
saber <saber.tabatabaee@gmail.com>
2025-01-31 18:58:33 +0330

    sharabe beheshti model relation

--- app/Models/SharabeBeheshtiMp3.php
+++ app/Models/SharabeBeheshtiMp3.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class SharabeBeheshtiMp3 extends Model
 {
     use HasFactory;
+
+
+    public function uploadedFiles()
+    {
+        return $this->morphMany(UploadedFile::class, 'model');
+    }
 }
