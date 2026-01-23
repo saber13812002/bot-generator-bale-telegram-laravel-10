60120f8254579227e8b7a020a7f35465d9aa9794
Saber Tabatabaee yazdi <saber.tabatabaee@gmail.com>
2026-01-23 21:01:22 +0100

    به‌روزرسانی متد `callTomorrow` در `WeatherTomorrowApiRepositoryImpl` با اصلاح URL برای محیط‌های غیر محلی. این تغییر به بهبود دسترسی به API کمک می‌کند و کد را از نظر خوانایی بهینه‌تر می‌سازد.

--- app/Repositories/WeatherTomorrowApiRepositoryImpl.php
+++ app/Repositories/WeatherTomorrowApiRepositoryImpl.php
@@ -26,7 +26,7 @@ class WeatherTomorrowApiRepositoryImpl implements WeatherTomorrowApiRepository
         $api_key = env("TOMORROW_API_TOKEN");
 
         $client = new GuzzleHttp\Client();
-        $baseUrl = env("APP_ENV") != "local" ? "https://api.tomorrow.io" : "http://localhost:3002";
+        $baseUrl = env("APP_ENV") != "local" ? "https://api.tomorrow.io" : "https://api.tomorrow.io";
         $uri = $baseUrl . '/v4/timelines?location=34.600209,50.828128&apikey=' . $api_key . '&units=metric&timesteps=1h&fields=temperature,windSpeed,windDirection,windGust,pressureSurfaceLevel,pressureSeaLevel,rainIntensity,visibility,cloudCover,uvIndex,humidity,weatherCode,temperatureApparent';
 
         $response = $client->get($uri);
