146fc18babfe794b5f8985502e2fe99e3f836dc8
saber13812002 <saber.tabatabaee@gmail.com>
2026-07-03 20:40:07 +0330

    تست های یونیت مرحله دوم هم رفع باگ شدند

--- app/Modules/BotOwner/Models/BotOwner.php
+++ app/Modules/BotOwner/Models/BotOwner.php
@@ -2,11 +2,14 @@
 
 namespace App\Modules\BotOwner\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class BotOwner extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'phone',
         'name',
